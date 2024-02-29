1f034c8c7e49a09d634e1c584afc2fa58cab106a
Swaranthi1111 <156526582+Swaranthi1111@users.noreply.github.com>
2024-02-29 21:55:11 +0530

    Update Users.java

--- Task4 OnlineExamination/Bean/Users.java
+++ Task4 OnlineExamination/Bean/Users.java
@@ -1,1 +1,49 @@
 
+package tasks.task4_Online_Examination.bean;
+
+public class User {
+    private String userName;
+    private String userPassword;
+    private long phoneNo;
+
+    public User(){}
+
+    public User(String userName, String userPassword,long phoneNo) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", phoneNo=" + phoneNo +
+                '}';
+    }
+}
