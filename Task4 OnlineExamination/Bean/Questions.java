d31ee6e5a5560f914e7e9dbe6e841a97d28579d1
Swaranthi1111 <156526582+Swaranthi1111@users.noreply.github.com>
2024-02-29 21:53:06 +0530

    Update Questions.java

--- Task4 OnlineExamination/Bean/Questions.java
+++ Task4 OnlineExamination/Bean/Questions.java
@@ -1,1 +1,43 @@
 
+package tasks.task4_Online_Examination.bean;
+
+import java.util.List;
+
+public class Questions {
+    private String questions;
+    private List<String> options;
+    private int correctAnswer;
+
+    public Questions() {
+    }
+
+    public Questions(String questions, List<String> options, int correctAnswer) {
+        this.questions = questions;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(String questions) {
+        this.questions = questions;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
